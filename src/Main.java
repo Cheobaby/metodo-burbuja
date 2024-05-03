fd3dee565bd740b078b2c3aa497c69a5794b9a0d
nol <noelmtzramirez1.6@gmail.com>
2024-05-03 17:50:07 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int []nums={2,1,5,3,2,4};
+        bubbleSort(nums);
+    }
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        showArray(arr);
+    }
+    public static void showArray(int []numbers){
+        for(int n:numbers){
+            System.out.print(n+",");
+        }
+    }
+
+}
